7b897457f0f52e75143c5819d3443804d47ff068
mira2k <dammitblin41@gmail.com>
2017-04-15 18:13:39 +0300

    mems+users.sql
    merged tables, added columns to bros

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,11 @@
+DROP TABLE public.bros;
+
+DROP TABLE public.mems;
+
+
 CREATE TYPE mem_type AS ENUM ('picture', 'video', 'gif', 'coub', 'pasta');
-CREATE TABLE mems
+
+CREATE TABLE memes
 (
 	mem_id integer PRIMARY KEY,
 	--pics
@@ -14,17 +20,16 @@ CREATE TABLE public.bros
 (
   bro_id integer PRIMARY KEY,
   name character varying(20) NOT NULL, -- Имя пользователя
+  password character varying(30), -- Пароль
+  fav_mem integer REFERENCES memes(mem_id), -- Любимый мем
   SP character varying(30), -- семейное положение
-  gender floor,
-  UNIQUE(bro_id)
+  occupation character varying(30), -- деятельность
+  city character varying(30), -- город
+  gender floor, --гендер пользователя
+  CHECK (length(password) >= 9)
 )
-
-CREATE TABLE memsages
-(
-	memsage_id integer PRIMARY KEY,
-	sender integer REFERENCES bros(bro_id) NOT NULL,
-	reciever integer REFERENCES bros(bro_id) NOT NULL,
-	send_time date NOT NULL,
-	attached_mem integer REFERENCES memes(mem_id) NOT NULL,
-	comment text
-)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.bros
+  OWNER TO postgres;
