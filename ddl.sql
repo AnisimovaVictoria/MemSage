f863ada670443f235c26a3f82a37f4c22374b2ac
AnisimovaVictoria <anisimova.vs@phystech.edu>
2017-04-15 17:31:26 +0300

    added table mems

--- ddl.sql
+++ ddl.sql
@@ -1,1 +1,10 @@
+CREATE TYPE mem_type AS ENUM ('picture', 'video', 'gif', 'coub', 'pasta');
 
+CREATE TABLE mems
+(
+	mem_id integer PRIMARY KEY,
+	--pics
+	name varchar(30) NOT NULL UNIQUE,
+	type() mem_type,
+	origin text DEFAULT "Who knows ¯\_(ツ)_/¯"
+);
