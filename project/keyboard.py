e4db12805cc48255edfd641bdfe2e1b27a5d126c
AnisimovaVictoria <anisimova.vs@phystech.edu>
2017-05-10 19:42:53 +0300

    added keyboard "mem"

--- project/keyboard.py
+++ project/keyboard.py
@@ -1,13 +1,13 @@
 from telebot import types
 
 
-start_buttons = ["Познакомиться", "Посмотреть мем", "Добавить мем"]
-mem_types = ['кошки', 'Пепе', 'мопсы', "Назад"]
+start_menu = ["Познакомиться", "Посмотреть мем", "Добавить мем"]
+mem_types = ['кошки', 'Пепе', 'мопсы', "Назад 🔙"]
 
 
 def start():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    for s in start_buttons:
+    for s in start_menu:
         markup.add(s)
     return markup
 
@@ -16,6 +16,14 @@ def choose_mem():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     for s in mem_types:
         markup.add(s)
+    return markup
+
+
+def mem():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True,
+                                       resize_keyboard=True)
+    markup.add("◀", "❤", "▶")
+    markup.add("Назад 🔙")
     return markup
 
 rem = types.ReplyKeyboardRemove
