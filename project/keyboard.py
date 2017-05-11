00bef48733ac77b118142b6e1d767c1096e8cfd9
AnisimovaVictoria <anisimova.vs@phystech.edu>
2017-05-11 20:49:27 +0300

    Update keyboard.py

--- project/keyboard.py
+++ project/keyboard.py
@@ -1,25 +1,12 @@
 from telebot import types
 
 
-start_menu = ["Посмотреть мем", "Добавить мем", "Найти друзей)"]
+main_menu = ["Посмотреть мем", "Добавить мем", "Найти друзей)"]
 mem_types = ['Хот', "Трендинг", 'Свежак', 'По категориям', "Назад 🔙"]
 mem_categories = ['Кошки', 'Пепе', 'Мопсы', "Назад 🔙"]
-
-
-def start():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True,
-                                       resize_keyboard=True)
-    for s in start_menu:
-        markup.add(s)
-    return markup
-
-
-def choose_mem():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True,
-                                       resize_keyboard=True)
-    for s in mem_types:
-        markup.add(s)
-    return markup
+gender_types = ['ЛИНОЛЕУМ', 'ЛАМИНАТ', 'КОВРОЛИН', 'КЕРАМОГРАНИТ', 'ПАРКЕТ',
+                'БРЕВЕНЧАТЫЙ', 'НАЛИВНОЙ']
+sp_types = ["forever alone", "IN LOVE", "Все сложно"]
 
 
 def mem():
@@ -29,6 +16,14 @@ def mem():
     markup.add("Назад 🔙")
     return markup
 
-rem = types.ReplyKeyboardRemove
 
-interview = [rem, rem, rem, rem, rem, start]
+def make_markup(args):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True,
+                                       resize_keyboard=True)
+    for s in args:
+        markup.add(s)
+    return markup
+
+
+rem = types.ReplyKeyboardRemove
+interview_markup = [rem, make_markup(gender_types), make_markup(sp_types), rem, rem, make_markup(main_menu)]
