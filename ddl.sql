5d50d2363970b7186c21390955617f63cc25ca40
mira2k <dammitblin41@gmail.com>
2017-05-13 13:28:35 +0300

    UPDATED MEMES, BROS, MEM_TYPE
    CREATED INDEX bro_id_idx ON BROS
    CHANGED MEM CATEGORIES

--- ddl.sql
+++ ddl.sql
@@ -1,19 +1,20 @@
-CREATE TYPE mem_type AS ENUM ('picture', 'video', 'gif', 'coub', 'pasta', 'text');
+CREATE TYPE mem_type AS ENUM ('pepe', 'cats', 'dogs');
 
 CREATE SEQUENCE auto_id_mems;
 CREATE TABLE public.memes
 (
   mem_id integer NOT NULL DEFAULT nextval('auto_id_mems'::regclass),
-  mem_path character varying(256),
+  file_id text NOT NULL default '0'::text,
   name character varying(30) NOT NULL,
-  type mem_type,
+  type mem_type DEFAULT 'pepe'::mem_type,
+  gustas integer DEFAULT 0,
   origin text DEFAULT 'Who knows ¯\_(ツ)_/¯'::text,
   CONSTRAINT memes_pkey PRIMARY KEY (mem_id),
   CONSTRAINT unique_name UNIQUE (name)
-);
+)
 
 CREATE TYPE floor AS ENUM ('linoleum', 'laminate', 'kovrolin', 'keramogranit', 'parquet', 'hardwood', 'self-leveling');
-CREATE TYPE sp_type AS ENUM ('forever alone', 'IN LOVE');
+CREATE TYPE sp_type AS ENUM ('FOREVER ALONE((', 'IN LOVE', 'ЕСТЬ ЕДА', 'ВСЕ ОЧЕНЬ СЛОЖНА');
 CREATE TYPE occup_type AS ENUM ('pre!shkolyar', 'shkolyar', 'fiztech!shkolyar', 'post!shkolyar');
 
 CREATE SEQUENCE auto_id_bros;
@@ -22,24 +23,22 @@ CREATE TABLE public.bros
 (
   bro_id integer NOT NULL DEFAULT nextval('auto_id_bros'::regclass),
   name character varying(20) NOT NULL,
-  fav_mem integer DEFAULT 0,
+  gender floor DEFAULT 'linoleum'::floor,
   sp sp_type DEFAULT 'forever alone'::sp_type,
   occupation occup_type DEFAULT 'shkolyar'::occup_type,
   city character varying(30),
-  gender floor DEFAULT 'linoleum'::floor,
+  is_hikka boolean DEFAULT true,
+  fav_mem mem_type DEFAULT 'pepe'::mem_type,
   CONSTRAINT bros_pkey PRIMARY KEY (bro_id)
-);
-  
-CREATE TABLE memsages
- (
- 	memsage_id integer PRIMARY KEY,
- 	sender integer REFERENCES bros(bro_id) NOT NULL,
- 	reciever integer REFERENCES bros(bro_id) NOT NULL,
- 	send_time date NOT NULL,
- 	attached_mem integer REFERENCES memes(mem_id) NOT NULL,
- 	comment text
- );
- 
+)
+-- DROP INDEX public.bro_id_idx;
+
+CREATE UNIQUE INDEX bro_id_idx
+  ON public.bros
+  USING btree
+  (bro_id);
+
+
 CREATE TYPE status_type AS ENUM ('follower', 'rejected', 'in black list');
 --первый подписан на второго, первый отказал в дружбе второму, первый добавил второго в черный литс
 CREATE TABLE relationships
@@ -56,12 +55,3 @@ CREATE TABLE megustas
 	bro_id integer REFERENCES bros(bro_id),
 	PRIMARY KEY (mem_id, bro_id)
 );
-
-CREATE TABLE posts
- (
-	post_id integer PRIMARY KEY,
- 	sender integer REFERENCES bros(bro_id) NOT NULL,
- 	postime date NOT NULL,
- 	attached_mem integer REFERENCES memes(mem_id) NOT NULL,
- 	comment text
- );
